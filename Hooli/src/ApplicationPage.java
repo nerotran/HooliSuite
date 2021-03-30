9e1962ac3200bcc144fd0188176077bbd6405870
Nero Tran <tranhuq@miamioh.edu>
2021-03-30 00:27:53 -0400

    ApplicationPage class

--- Hooli/src/ApplicationPage.java
+++ Hooli/src/ApplicationPage.java
@@ -1,4 +1,13 @@
+import javax.swing.JFrame;
 
-public class ApplicationPage {
+public class ApplicationPage extends JFrame{
+	private Application app;
+	
+	public ApplicationPage(Application app) {
+		super();
+		this.app = app;
+		this.setBounds(50, 50, 800, 600);
+		this.setResizable(true);
+	}
 
 }
