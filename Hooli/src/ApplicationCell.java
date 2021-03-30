d4408d88608be19b7d6476c52b12a2a2fdc7b37d
Nero Tran <tranhuq@miamioh.edu>
2021-03-30 00:53:14 -0400

    Custom look for the ApplicationList entry

---
+++ Hooli/src/ApplicationCell.java
@@ -0,0 +1,46 @@
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+public class ApplicationCell extends JLabel implements ListCellRenderer<Application> {
+	Color background;
+	Color foreground;
+	
+	public ApplicationCell() {
+		setOpaque(true);
+	}
+
+	@Override
+	public Component getListCellRendererComponent(JList<? extends Application> list, Application value, int index,
+			boolean isSelected, boolean cellHasFocus) {
+		// TODO Auto-generated method stub
+		this.setText(value.getName() + "   by  " + value.getPublisher());
+		JList.DropLocation dropLocation = list.getDropLocation();
+        if (dropLocation != null
+                && !dropLocation.isInsert()
+                && dropLocation.getIndex() == index) {
+
+            background = Color.BLUE;
+            foreground = Color.WHITE;
+
+        // check if this cell is selected
+        } else if (isSelected) {
+            background = Color.RED;
+            foreground = Color.WHITE;
+
+        // unselected, and not the DnD drop location
+        } else {
+            background = Color.WHITE;
+            foreground = Color.BLACK;
+        };
+
+        this.setBackground(background);
+        this.setForeground(foreground);
+
+        return this;
+	}
+
+}
