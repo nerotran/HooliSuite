42ea1329207cbdd214c9545b5765a6a07136657f
Nero Tran <tranhuq@miamioh.edu>
2021-03-30 01:16:20 -0400

    ApplicationPage class

--- Hooli/src/ApplicationPage.java
+++ Hooli/src/ApplicationPage.java
@@ -1,7 +1,21 @@
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Desktop;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-public class ApplicationPage extends JFrame{
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ApplicationPage extends JFrame implements MouseListener {
 	private Application app;
+	private JLabel name, publisher, platform, description, price, date;
+	private JButton link;
+	private JPanel info;
 	
 	public ApplicationPage(Application app) {
 		super();
@@ -9,7 +23,86 @@ public class ApplicationPage extends JFrame{
 		this.setBounds(50, 50, 800, 600);
 		this.setResizable(true);
 		this.setAlwaysOnTop(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		graphics();
+	}
+	
+	private void graphics() {
+		info = new JPanel();
+		info.setLayout(new BoxLayout(info, BoxLayout.Y_AXIS));
+		
+		name = new JLabel("Name: " + this.app.getName());
+		info.add(name);
+		
+		publisher = new JLabel("Publisher: " + this.app.getPublisher());
+		info.add(publisher);
+
+		platform = new JLabel("Platform: " + this.app.getPlatform());
+		info.add(platform);
+
+		price = new JLabel("Price: " + this.app.getPrice());
+		info.add(price);
+			
+		date = new JLabel("Released: " + this.app.getDate());
+		info.add(date);
+		
+		link = new JButton("Link");
+		link.addMouseListener(this);
+		info.add(link);
+		
+		description = new JLabel("Description: " + this.app.getDescription());
+		info.add(description);
+		
+		this.add(info);
+
+	}
+	
+	public static boolean openWebpage(URI uri) {
+	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+	        try {
+	            desktop.browse(uri);
+	            return true;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return false;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		try {
+			openWebpage(new URI("https://www.google.com/"));
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
