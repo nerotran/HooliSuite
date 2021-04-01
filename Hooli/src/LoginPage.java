137c7e371590555b4611f852241b6784ca15f116
Nero Tran <tranhuq@miamioh.edu>
2021-04-01 16:48:14 -0400

    Changed to BoxLayout

--- Hooli/src/LoginPage.java
+++ Hooli/src/LoginPage.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,8 +25,7 @@ public class LoginPage extends JFrame implements ActionListener {
 	
 	public void buildPage() {
 		panel = new JPanel();
-		GroupLayout layout = new GroupLayout(panel);
-		panel.setLayout(layout);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 		userName = new JLabel("Username: ");
 		password = new JLabel("Password: ");
