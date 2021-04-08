89a47bfce4b5f32e6ae113c3fb426fc42c0d8c69
Nero Tran <tranhuq@miamioh.edu>
2021-04-08 15:20:59 -0400

    Add test case for Application class

---
+++ Hooli/src/ApplicationTest.java
@@ -0,0 +1,73 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationTest {
+
+	@Test
+	void testApplicationStringString() {
+		Application a = new Application ("H1", "Hooli");
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+	}
+
+	@Test
+	void testApplicationStringStringString() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.");
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+		assertEquals(a.getPlatform(), "PC");
+		assertEquals(a.getDescription(), "This our CSE201 project.");
+	}
+
+	@Test
+	void testApplicationStringStringStringString() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.");
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+		assertEquals(a.getPlatform(), "PC");
+		assertEquals(a.getDescription(), "This our CSE201 project.");
+	}
+
+	@Test
+	void testApplicationStringStringStringStringDouble() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.", 10);
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+		assertEquals(a.getPlatform(), "PC");
+		assertEquals(a.getDescription(), "This our CSE201 project.");
+		assertEquals(a.getPrice(), "$10.00");
+	}
+
+	@Test
+	void testApplicationStringStringStringStringDoubleString() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.", 10, "https://www.google.com");
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+		assertEquals(a.getPlatform(), "PC");
+		assertEquals(a.getDescription(), "This our CSE201 project.");
+		assertEquals(a.getPrice(), "$10.00");
+		assertEquals(a.getLink(), "https://www.google.com");
+	}
+
+	@Test
+	void testApplicationStringStringStringStringDoubleStringString() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.", 10, "https://www.google.com", "4/9/2021");
+		assertEquals(a.getName(), "H1");
+		assertEquals(a.getPublisher(), "Hooli");
+		assertEquals(a.getPlatform(), "PC");
+		assertEquals(a.getDescription(), "This our CSE201 project.");
+		assertEquals(a.getPrice(), "$10.00");
+		assertEquals(a.getLink(), "https://www.google.com");
+		assertEquals(a.getDate(), "4/9/2021");
+	}
+
+	@Test
+	void testToString() {
+		Application a = new Application ("H1", "Hooli", "PC", "This our CSE201 project.", 10, "https://www.google.com", "4/9/2021");
+		assertEquals("Name: H1 Publisher: Hooli Platform: PC Description:"
+				+ " This our CSE201 project. Price: 10.0 Link: https://www.google.com Released: 4/9/2021", a.toString());
+	}
+
+
+}
