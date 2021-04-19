8ecdb812f14bd51fa7513292077ecf2f3a2d5cf2
Scott Williams <will1181@miamioh.edu>
2021-04-19 19:07:45 -0400

    Fixed test file

--- Hooli/src/FileReaderTest.java
+++ Hooli/src/FileReaderTest.java
@@ -1,17 +1,19 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 
 class FileReaderTest {
 
 	@Test
 	void testReadAppFile() {
-		Application[] test = FileReader.readAppFile("ApplicationData.txt");
-		assertEquals(test[0].toString(), "Name: Ping Pong Publisher: Java Platform: PC "
+		ArrayList<Application> test = FileReader.readAppFile("ApplicationData.txt");
+		assertEquals(test.get(0).toString(), "Name: Ping Pong Publisher: Java Platform: PC "
 				+ "Description: Desc Price: 100.0 Link: https://www.google.com/ Released: ");
-		assertEquals(test[1].toString(), "Name: Solitaire Publisher: Linux Platform: XBox "
+		assertEquals(test.get(1).toString(), "Name: Solitaire Publisher: Linux Platform: XBox "
 				+ "Description: Desc Price: 200.0 Link: https://www.google.com/ Released: ");
-		assertEquals(test[2].toString(), "Name: Tetris Publisher: Microsoft Platform: PC "
+		assertEquals(test.get(2).toString(), "Name: Tetris Publisher: Microsoft Platform: PC "
 				+ "Description: Desc Price: 55.0 Link: https://www.google.com/ Released: ");
 	}
 
