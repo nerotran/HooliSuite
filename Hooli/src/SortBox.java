ee8207aea4535f1604b35c5ba7b5605e81c18a79
Nero Tran <tranhuq@miamioh.edu>
2021-04-18 22:57:42 -0400

    Add sort function

---
+++ Hooli/src/SortBox.java
@@ -0,0 +1,45 @@
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class SortBox extends JComboBox implements ItemListener {
+	ArrayList<Application> list;
+	JFrame frame;
+
+	public SortBox(ArrayList<Application> list, JFrame frame) {
+		this.list = list;
+		this.frame = frame;
+		this.addItem("[Sort By: ] ");
+		this.addItem("Name");
+		this.addItem("Publisher");
+		this.addItem("Date Added");
+		this.addItem("Price");
+		this.addItemListener(this);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent arg0) {
+		// TODO Auto-generated method stub
+		String getItem = (String)this.getSelectedItem();
+		
+		if (getItem.equals("Name")) {
+			list.sort(Comparator.comparing(Application::getName));
+		}
+		if (getItem.equals("Publisher")) {
+			list.sort(Comparator.comparing(Application::getPublisher));
+		}
+		if (getItem.equals("Date Added")) {
+			list.sort(Comparator.comparing(Application::getDate));
+		}
+		if (getItem.equals("Price")) {
+			list.sort(Comparator.comparing(Application::getPrice));
+		}
+		
+		frame.repaint();
+	}
+}
