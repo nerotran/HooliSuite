8778f169a5e9d4b783d3b2f2e607ddf490d27026
Joe Janaskie <janaskjc@miamioh.edu>
2021-04-19 14:44:01 -0400

    Updated Search

--- Hooli/src/Search.java
+++ Hooli/src/Search.java
@@ -1,54 +1,57 @@
+import java.util.ArrayList;
 
 public class Search {
-	
+
 	private String string;
-	private Application[] list;
-	
-	public Search(String s, Application[] l) {
+	private ArrayList<Application> list;
+	private ArrayList<Application> finList;
+
+	public Search(String s, ArrayList<Application> l) {
 		string = s;
 		list = l;
+		finList = new ArrayList<>();
 		check(string, list);
 	}
-	
-	public boolean check(String s, Application[] appList) {
-		for (int i = 0; i < appList.length; i++) {
-			if (appList[i].getDate().toUpperCase().contains(s.toUpperCase())) {
-				return true;
+
+	public void check(String s, ArrayList<Application> appList) {
+		int count = 0;
+		for (int i = 0; i < appList.size(); i++) {
+//			if (appList.get(i).getDate().toString().toUpperCase().contains(s.toUpperCase())) {
+//				finList.add(appList.get(i));
+//				return true;
+//			}
+			if (appList.get(i).getName().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
-			if (appList[i].getName().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
+			if (appList.get(i).getDescription().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
-			if (appList[i].getDescription().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
+			if (appList.get(i).getLink().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
-			if (appList[i].getLink().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
+			if (appList.get(i).getPlatform().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
-			if (appList[i].getPlatform().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
+			if (appList.get(i).getPrice().toString().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
-			if (appList[i].getPrice().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
-			}
-			if (appList[i].getPublisher().toUpperCase().contains(s.toUpperCase())) {
-				System.out.println(appList[i]);
-				return true;
+			if (appList.get(i).getPublisher().toUpperCase().contains(s.toUpperCase())) {
+				finList.add(appList.get(i));
 			}
 		}
-		
-		return false;
-	}
-	
-	public Application[] newList() {
-		
-		Application[] t = new Application[10];
-		t[0] = new Application("hi", "yo", "there");
-		return t;
 	}
 
+
+	public ArrayList<Application> originalList() {
+		return list;
+	}
+	public ArrayList<Application> newList() {
+		return finList;
+	}
+
+
 }
+
+
+
+
