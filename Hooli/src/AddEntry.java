444ca69477220424ef51f4130a6c4d6dd4af41c4
Scott Williams <will1181@miamioh.edu>
2021-05-04 15:10:28 -0400

    Fixed functionality

--- Hooli/src/AddEntry.java
+++ Hooli/src/AddEntry.java
@@ -1,10 +1,20 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -26,12 +36,14 @@ public class AddEntry extends JFrame implements ActionListener {
 	private JTextField descInfo;
 	private JTextField linkInfo;
 	private JButton addBut;
+	Set<Application> requests;
 	
 	public AddEntry() {
 		super("Add Entry");
 		this.setBounds(450, 400, 700, 600);
 		this.setResizable(true);
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		requests = pullRequestInfo("Request.csv");
 	}
 	
 	public void buildPage() {
@@ -41,9 +53,9 @@ public class AddEntry extends JFrame implements ActionListener {
 		
 		name = new JLabel("Name: ");
 		publish = new JLabel("Publisher: ");
-		platform = new JLabel("Platform: ");
-		price = new JLabel("Price: ");
-		release = new JLabel("Released: ");
+		platform = new JLabel("Platform(s): ");
+		price = new JLabel("Price (Example 2.0; No $): ");
+		release = new JLabel("Released (Format xx/xx/xxxx): ");
 		desc = new JLabel("Description: ");
 		link = new JLabel("Link: ");
 		nameInfo = new JTextField(10);
@@ -72,12 +84,91 @@ public class AddEntry extends JFrame implements ActionListener {
 		layout.setVgap(3);
 		panel.add(addBut);
 		
+		addBut.addActionListener(this);
 		this.add(panel);
 	}
-
+	
+	public void writeToFile(String fileName, Set<Application> data) {
+		File file = new File(fileName);
+		try {
+			PrintWriter output = new PrintWriter(file);
+			output.append("Name, Publisher, Platform, Description, Price, Link, Release, \n");
+			
+			for (Application app : data) {
+				output.append(app.getName() + "," + app.getPublisher() + "," + app.getPlatform() + "," + 
+						app.getDescription() + "," + app.getPrice() + "," + app.getLink() + "," + app.getDate() + ",\n");
+			}
+			
+			output.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found");
+		}
+		
+	}
+	
+	public static Set<Application> pullRequestInfo(String fileName) {
+		File file = new File(fileName);
+		String appData = "";
+		Set<Application> requests = new HashSet<>();
+		try {
+			Scanner input = new Scanner(file);
+			if (input.hasNextLine()) {
+				input.nextLine();
+			}
+			while (input.hasNext()) {
+				appData = appData + input.next();
+			}
+			
+			String[] userArray = appData.split(",");
+			for (int i = 0; i+6 < userArray.length; i+=7) {
+				Application user = new Application(userArray[i], userArray[i+1], userArray[i+2], (userArray[i+3]), 
+						Double.parseDouble(userArray[i+4]), userArray[i+5], userArray[i+6]);
+				requests.add(user);
+			}
+			input.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("File not found");
+		}
+		
+		return requests;
+	}
+	
+	/**
+	 * 
+	 * @param name
+	 * @param publish
+	 * @param plat
+	 * @param price
+	 * @param release
+	 * @param desc
+	 * @param link
+	 * @return
+	 */
+	public boolean textFieldEmpty(String name, String publish, String plat, String price, String release,
+			String desc, String link) {
+		if (name.equals("") || publish.equals("") || plat.equals("") || price.equals("") || 
+				release.equals("") || desc.equals("") || link.equals("")) {
+			JOptionPane.showMessageDialog(this, "One or more field is empty");
+			return true;
+		}
+		
+		return false;
+	}
+	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource().equals(addBut)) {
+			if (!textFieldEmpty(nameInfo.getText(), publishInfo.getText(), platformInfo.getText(), priceInfo.getText(),
+					releaseInfo.getText(), descInfo.getText(), linkInfo.getText()) && !priceInfo.getText().contains("$")) {
+				Application request = new Application(nameInfo.getText(), publishInfo.getText(), platformInfo.getText(), 
+						descInfo.getText(), Double.parseDouble(priceInfo.getText()), linkInfo.getText(), releaseInfo.getText());
+				requests.add(request);
+				writeToFile("Request.csv", requests);
+				this.setVisible(false);
+				JOptionPane.showMessageDialog(this, "Request sent to Admins");
+			}
+			
+		}
 		
 	}
 
